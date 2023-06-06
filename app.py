bf91818f5f9258397a0c43b9576a5dfced6c48d8
Olamide <olire96@gmail.com>
2023-06-06 19:24:33 +0000

    modifications

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/afrobeatstats/', strict_slashes=False)
 def index():
     # Load the contents of file_storage.json
     with open('file_storage.json', 'r') as file:
         artists = json.load(file)
 
     # Render the modified template with the loaded data
-    return render_template('index.html', artists=artists)
+    return render_template('topartists.html', artists=artists)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5009)
